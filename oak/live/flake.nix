bb2f7b2342f5071ce1646c5200e43784eb01b7c8
Magnus Hammar <magnus@tenk.se>
2024-04-28 17:14:38 +0200

    please let it be just the flake that's f'd

--- oak/live/flake.nix
+++ oak/live/flake.nix
@@ -2,10 +2,6 @@
   description = "NixOS configuration";
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-23.11";
-    home-manager = {
-      url = "github:nix-community/home-manager/release-23.11";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
   };
   outputs = { self, nixpkgs, home-manager, ... }:
     let
@@ -19,14 +15,7 @@
         oak = nixpkgs.lib.nixosSystem {
           inherit system;
           modules = [
-            ./oak-live-configuration.nix
-            home-manager.nixosModules.home-manager {
-              home-manager.useGlobalPkgs = true;
-              home-manager.useUserPackages = true;
-              home-manager.users.hammar = {
-                imports = [ ./oak-live-home.nix ];
-              };
-            }
+            ./configuration.nix
           ];
         };
       };
