9bbc080d0e1be26136201a1f5671814c6f5ed2dd
Magnus Hammar <magnus@tenk.se>
2024-04-28 19:32:10 +0200

    Live version with home manager

---
+++ oak/live-homemanager/configuration.nix
@@ -0,0 +1,162 @@
+{ config, pkgs, lib, ... }:
+
+let
+  # Determine the base path based on the existence of /mnt/etc/nixos
+  basePath = if builtins.pathExists "/mnt/etc/nixos/configuration.nix" then "/mnt" else "";
+  jsonPath = "${basePath}/etc/nixos/systemParams.json";
+
+  # Read the JSON file and decode it
+  jsonContent = builtins.readFile jsonPath;
+  systemParams = builtins.fromJSON jsonContent;
+in
+{
+  ########## Local File Systems ##########
+  boot.loader.grub = {
+    enable = true;
+    zfsSupport = true;
+    efiSupport = true;
+    efiInstallAsRemovable = true;
+    mirroredBoots = [
+      { devices = [ "nodev"]; path = "/boot"; }
+    ];
+  };
+  
+  # Specifically for ZFS
+  networking.hostId = "${systemParams.networkingHostId}";
+
+  fileSystems."/" = { device = "zpool/root"; fsType = "zfs"; };
+  fileSystems."/nix" = { device = "zpool/nix"; fsType = "zfs"; };
+  fileSystems."/var" = { device = "zpool/var"; fsType = "zfs"; };
+  fileSystems."/home" = { device = "zpool/home"; fsType = "zfs"; };
+  fileSystems."/home/hammar/data" = { device = "zpool/home/data"; fsType = "zfs"; };
+  fileSystems."/home/hammar/github" = { device = "zpool/home/github"; fsType = "zfs"; };
+  fileSystems."/home/hammar/clickhouse" = { device = "zpool/home/clickhouse"; fsType = "zfs"; };
+  fileSystems."/boot" = { device = "/dev/disk/by-uuid/${systemParams.espUUID}"; fsType = "vfat"; };
+
+  services.zfs.autoScrub.enable = true;
+  services.zfs.autoScrub.interval = "weekly";
+
+  swapDevices = [ ];
+  
+  ########## Network File Systems ##########
+
+  fileSystems."/mnt/rygel/files" = {
+    device = "//rygel/files";
+    fsType = "cifs";
+    options = [
+      "credentials=/etc/nixos/hammar-nixos-cfg/rygelcredentials"
+      "uid=${toString config.users.users.hammar.uid}"
+      "gid=${toString config.users.groups.users.gid}"
+      "file_mode=0660"
+      "dir_mode=0770"
+      "noauto"
+      "x-systemd.automount"
+    ];
+  };
+
+  fileSystems."/mnt/rygel/arkiv" = {
+    device = "//rygel/arkiv";
+    fsType = "cifs";
+    options = [
+      "credentials=/etc/nixos/hammar-nixos-cfg/rygelcredentials"
+      "uid=${toString config.users.users.hammar.uid}"
+      "gid=${toString config.users.groups.users.gid}"
+      "file_mode=0660"
+      "dir_mode=0770"
+      "noauto"
+      "x-systemd.automount"
+    ];
+  };
+
+  nixpkgs.config.allowUnfree = true;
+  nixpkgs.config.permittedInsecurePackages = [
+    "electron-25.9.0"
+  ];
+
+  ########## NIX  ##########
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+
+  ########## GRAPHICS ##########
+  services.xserver.videoDrivers = [ "nvidia" ];
+  hardware.nvidia.modesetting.enable = true;
+
+  hardware.opengl = {
+    enable = true;
+    driSupport = true;
+    driSupport32Bit = true;
+  };
+
+  ########## DESKTOP ENVIRONMENT ##########
+  services.xserver.enable = true;
+  services.xserver.desktopManager.plasma5.enable = true;
+  services.xserver.displayManager.sddm.enable = true;
+  services.xserver.displayManager.sddm.wayland.enable = false;
+
+  ########## REGIONAL & LOCALE ##########
+
+  services.xserver.xkbOptions = "eurosign:e";
+  services.xserver.xkb.layout = "se";
+  time.timeZone = "Europe/Stockholm";
+  services.timesyncd.enable = true;
+  i18n.defaultLocale = "sv_SE.UTF-8";
+
+  ########## OTHER HARDWARE ##########
+
+  sound.enable = true;
+  ## hardware.pulseaudio.enable = true;
+  hardware.pulseaudio = {
+  enable = true;
+  extraConfig = ''
+    set-default-sink alsa_output.pci-0000_00_1b.0.analog-stereo
+    set-default-source alsa_input.pci-0000_00_1b.0.analog-stereo
+  '';
+};
+
+  ########## NETWORKING ##########
+
+  networking.networkmanager.enable = true;
+
+  environment.etc."nsswitch.conf".text = ''
+   hosts: files mdns_minimal [NOTFOUND=return] dns mdns
+  '';
+ 
+  services.avahi.enable = true;
+  services.avahi.nssmdns = true;
+
+  networking.hosts = {
+    "192.168.68.52" = ["rygel"];
+    "192.168.68.54" = ["thermia"];
+    "127.0.0.1" = ["localhost"];
+    "127.0.1.1" = ["oak"];
+  };
+
+  ########## SOFTWARE ##########
+  
+ nixpkgs.config.allowUnfreePredicate = pkg: builtins.elem (lib.getName pkg) [
+   "nvidia-x11"
+   "nvidia-settings"
+ ];
+
+  virtualisation.docker.enable = true;
+
+  environment.systemPackages = with pkgs; [
+    kate
+    git
+    wget
+    dotnet-sdk_8 # keep other versions in nix-shells
+    samba
+    docker
+    cifs-utils
+    zip
+    lsof
+  ];
+
+  users.users.hammar = {
+    isNormalUser = true;
+    extraGroups = [ "wheel" ]; # Enable ‘sudo’ for the user.
+    initialHashedPassword = "${systemParams.hashedPassword}";
+  };
+
+  system.stateVersion = "23.11";
+
+}
